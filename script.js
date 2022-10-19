259b4ec936a91ff4cd2a7e2650ecfffd0dd354b7
wyattadkins@icloud.com <wyattadkins@icloud.com>
2022-10-19 14:22:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// 1
+
+for (i = 1; i < 8; i++){
+    console.log(i);
+}
+
+// 2
+for (i = 5; i <= 25; i += 4) {
+    console.log(i);
+}
+
+// 3a
+const wizards = [`Harry Potter`, `Hermoine Granger`,`Ron Weasley`];
+
+// 3b 
+for (chain of wizard){
+    console.log(chain);
+}
+
+// 4a
+let harryPotterMovies = 0
+// 4b
+// and
+// 4c
+while (harryPotterMovies <= 8){
+    console.log(harryPotterMovies);
+    number++;
+}
+
+console.log (harryPotterMovies);
+
+// BONUS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
